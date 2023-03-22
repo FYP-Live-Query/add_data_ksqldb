691f4d7b0426b450bfc5da945fe77941a2c3f092
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2023-03-22 20:51:08 +0530

    Implement adding data to kafka stream

--- add_data_ksqldb.py
+++ add_data_ksqldb.py
@@ -1,29 +1,30 @@
 from ksql import KSQLAPI
-from ksql.errors import KSQLAPIError
 
 class MyClass:
     def __init__(self, ksql_url):
         self.api_client = KSQLAPI(ksql_url)
 
     def insert_into_stream(self, stream_name, rows):
-        try:
-            # Construct the SQL query
-            values = ', '.join(f"({', '.join(str(value) for value in row.values())})" for row in rows)
-            query = f"INSERT INTO {stream_name} ({', '.join(rows[0].keys())}) VALUES {values}"
-
+        
+        for row in rows:
+        # Construct the SQL query
+            values = f"({', '.join(str(value) for value in row.values())})"
+            print(values)
+            query = f"INSERT INTO {stream_name} ({', '.join(rows[0].keys())}) VALUES {values};"
+            print(query)
             # Execute the SQL query
-            response = self.api_client.ksql(query)
-            return response
-
-        except KSQLAPIError as e:
-            print(f"Error inserting data into stream: {e}")
-            return None
-
+            try:
+                self.api_client.ksql(query)
+            except Exception as e:
+                pass
+        return None
 
 rows = [
-    {"PROFILEID": 'm2309eec', "LATITUDE": 37.7877, "LONGITUDE": -122.4011},
-    {"PROFILEID": 'k2309eec', "LATITUDE": 37.7877, "LONGITUDE": -122.4011}
+    {"PROFILEID": "\'m2309eec\'", "LATITUDE": 37.7877, "LONGITUDE": -122.4011},
+    {"PROFILEID": "\'k2309eec\'", "LATITUDE": 37.7877, "LONGITUDE": -122.4011},
+    {"PROFILEID": "\'k2309eec\'", "LATITUDE": 37.7877, "LONGITUDE": -122.4011}
 ]
 
-my_object = MyClass(ksql_url='http://localhost:8088')
+my_object = MyClass(ksql_url='http://10.8.100.246:8088')
 response = my_object.insert_into_stream(stream_name='riderLocations', rows=rows)
+
