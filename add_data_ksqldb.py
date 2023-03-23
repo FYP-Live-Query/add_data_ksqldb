75245040f1a6b9b7af47b74b49f244b4b62af1ae
vinoja98 <vinoja.18@cse.mrt.ac.lk>
2023-03-23 16:33:31 +0530

    add current timestamp

--- add_data_ksqldb.py
+++ add_data_ksqldb.py
@@ -1,17 +1,44 @@
+# from ksql import KSQLAPI
+
+# class MyClass:
+#     def __init__(self, ksql_url):
+#         self.api_client = KSQLAPI(ksql_url)
+
+#     def insert_into_stream(self, stream_name, rows):
+        
+#         for row in rows:
+#         # Construct the SQL query
+#             values = f"({', '.join(str(value) for value in row.values())})"
+#             query = f"INSERT INTO {stream_name} ({', '.join(rows[0].keys())}) VALUES {values};"
+#             # Execute the SQL query
+#             try:
+#                 self.api_client.ksql(query)
+#             except Exception as e:
+#                 pass
+#         return None
+
+# rows = [
+#     {"PROFILEID": "\'m2309eec\'", "LATITUDE": 37.7877, "LONGITUDE": -122.4011},
+#     {"PROFILEID": "\'k2309eec\'", "LATITUDE": 37.7877, "LONGITUDE": -122.4011},
+#     {"PROFILEID": "\'k2309eec\'", "LATITUDE": 37.7877, "LONGITUDE": -122.4011}
+# ]
+
+# my_object = MyClass(ksql_url='http://10.8.100.246:8088')
+# response = my_object.insert_into_stream(stream_name='riderLocations', rows=rows)
+
 from ksql import KSQLAPI
+from datetime import datetime
 
 class MyClass:
     def __init__(self, ksql_url):
         self.api_client = KSQLAPI(ksql_url)
 
     def insert_into_stream(self, stream_name, rows):
-        
         for row in rows:
-        # Construct the SQL query
-            values = f"({', '.join(str(value) for value in row.values())})"
-            print(values)
-            query = f"INSERT INTO {stream_name} ({', '.join(rows[0].keys())}) VALUES {values};"
-            print(query)
+            # Construct the SQL query
+            values = f"({', '.join(str(value) for value in row.values())}, CURRENT_TIMESTAMP)"
+            query = f"INSERT INTO {stream_name} ({', '.join(rows[0].keys())}, EVENTTIMESTAMP) VALUES {values};"
+            
             # Execute the SQL query
             try:
                 self.api_client.ksql(query)
@@ -22,9 +49,10 @@ class MyClass:
 rows = [
     {"PROFILEID": "\'m2309eec\'", "LATITUDE": 37.7877, "LONGITUDE": -122.4011},
     {"PROFILEID": "\'k2309eec\'", "LATITUDE": 37.7877, "LONGITUDE": -122.4011},
-    {"PROFILEID": "\'k2309eec\'", "LATITUDE": 37.7877, "LONGITUDE": -122.4011}
 ]
 
 my_object = MyClass(ksql_url='http://10.8.100.246:8088')
 response = my_object.insert_into_stream(stream_name='riderLocations', rows=rows)
+
+
 
