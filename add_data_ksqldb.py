fbf4a544c570576ea94f48c17797b58a53e9daac
vinoja98 <vinoja.18@cse.mrt.ac.lk>
2023-04-23 21:30:36 +0530

    Implement adding multiple data using single insert

--- add_data_ksqldb.py
+++ add_data_ksqldb.py
@@ -6,20 +6,36 @@ class MyClass:
         self.api_client = KSQLAPI(ksql_url)
     for i in range(2):
         def insert(self, stream_name, rows):
-            
+            values = []
             for row in rows:
                 # Add the current time as the eventTimestamp attribute
                 row["eventTimestamp"] = f"'{datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]}'"
-                # Construct the SQL query
-                values = f"({', '.join(str(value) for value in row.values())})"
-                query = f"INSERT INTO {stream_name} ({', '.join(rows[0].keys())}) VALUES {values};"
-                print(query)
-                # Execute the SQL query
-                try:
-                    self.api_client.ksql(query)
-                except Exception as e:
-                    pass
+                # Add the values of each row to the list
+                values.append(f"({', '.join(str(value) for value in row.values())})")
+            # Construct the SQL query with all the values
+            query = f"INSERT INTO {stream_name} ({', '.join(rows[0].keys())}) VALUES {', '.join(values)};"
+            print(query)
+            # Execute the SQL query
+            try:
+                self.api_client.ksql(query)
+            except Exception as e:
+                pass
             return None
+
+            
+            # for row in rows:
+            #     # Add the current time as the eventTimestamp attribute
+            #     row["eventTimestamp"] = f"'{datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]}'"
+            #     # Construct the SQL query
+            #     values = f"({', '.join(str(value) for value in row.values())})"
+            #     query = f"INSERT INTO {stream_name} ({', '.join(rows[0].keys())}) VALUES {values};"
+            #     print(query)
+            #     # Execute the SQL query
+            #     try:
+            #         self.api_client.ksql(query)
+            #     except Exception as e:
+            #         pass
+            # return None
     #for table
 rows = [
     {"ID":1,"IP": "\'108.23.85.jfd\'", "DATE": "\'2017-06-30\'", "TIME": "\'00:00:00\'", "BROWSER": "\'chr\'"},
